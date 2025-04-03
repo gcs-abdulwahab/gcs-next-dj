d7095f4328f624b170038bb21965e3aaa30a02a4
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2025-04-03 17:09:39 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@
 
 export default function Home() {
   
-  const name = "Something is changed over here";
+  const name1 = "Something is changed over here";
 
   return (
-    <section className="text-gray-600 body-font relative">
+    <section className="text-gray-600 body-font relative ">
       <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
         
